83cd49fd66185ef645bd0c4b50872296b8911206
Shaw Chang <digitalkc408@gmail.com>
2016-10-09 19:42:29 -0700

    Added a placeholder fragment

--- app/src/main/java/com/example/ken/sunshineapp/MainActivity.java
+++ app/src/main/java/com/example/ken/sunshineapp/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.ken.sunshineapp;
 
+import android.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
     }
 }
